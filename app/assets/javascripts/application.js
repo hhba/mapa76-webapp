97cb742cd728ef3559373caaaf0961a6de191bb8
malev <marcosvanetta@gmail.com>
2012-09-12 12:10:58 -0300

    Add backbone files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,8 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require_tree ./plugins
+//= require jquery.ui.draggable
+//= require jquery.ui.droppable
 //= require underscore
 //= require backbone
 //= require_tree ../templates/
@@ -21,4 +23,3 @@
 //= require_tree ./collections
 //= require_tree ./views
 //= require_tree ./routers
-//= require_tree .
