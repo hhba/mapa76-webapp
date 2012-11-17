bfb880756b7974127788e60c7f727ab0c7d653bc
Damián Silvani <munshkr@gmail.com>
2012-11-17 17:09:20 -0300

    Replace Bignum for Fixnum on timestamp test for NamedEntity

--- test/unit/named_entity_test.rb
+++ test/unit/named_entity_test.rb
@@ -29,7 +29,7 @@ class NamedEntityTest < ActiveSupport::TestCase
     end
 
     should "Retrieve timestamp" do
-      assert_instance_of Bignum, @date_entity_1.timestamp
+      assert_instance_of Fixnum, @date_entity_1.timestamp
     end
 
     should "tell you when it does not have a parseable date" do
