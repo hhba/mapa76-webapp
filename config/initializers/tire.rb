d9e8b37e36a34b0367020607360434276e071d67
Damián Silvani <munshkr@gmail.com>
2013-03-14 18:36:06 -0300

    Configure Tire logger as specified in elasticsearch.yml file

--- config/initializers/tire.rb
+++ config/initializers/tire.rb
@@ -2,3 +2,9 @@ es_config = YAML.load_file(File.join(Rails.root, "config", "elasticsearch.yml"))
 config = es_config[Rails.env]
 
 Tire::Model::Search.index_prefix config["index_prefix"]
+
+Tire.configure do
+  if config["log"]
+    logger File.expand_path(config["log"], Rails.root)
+  end
+end
