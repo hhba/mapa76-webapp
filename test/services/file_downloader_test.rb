fae5cb2d64886007f4299210bd8b03a0fcaa8fad
malev <marcosvanetta@gmail.com>
2013-04-28 20:04:23 -0300

    Add file downloader service

---
+++ test/services/file_downloader_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class FileDownladerTest < ActiveSupport::TestCase
+  setup do
+   @link = "http://malev.com.ar/images/rss-logo.png"
+  end
+
+  should "provide filename" do
+    file_dowloader = FileDownloader.new @link
+    assert_equal file_dowloader.filename, "rss-logo.png"
+  end
+
+  should "Download a file" do
+    file_dowloader = FileDownloader.new @link 
+    assert_instance_of Tempfile, file_dowloader.download
+  end
+end
