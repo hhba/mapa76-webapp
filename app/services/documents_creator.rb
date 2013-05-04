4180c88313317a39bcb8b2ebb90283c8a8ede4e0
malev <marcosvanetta@gmail.com>
2013-05-04 13:43:15 -0300

    Privatize pick_strategy method
    I'm pretty sure I'm not suppose to use Privatize
    in this context.

--- app/services/documents_creator.rb
+++ app/services/documents_creator.rb
@@ -5,11 +5,6 @@ class DocumentsCreator
     @strategy = pick_strategy
   end
 
-  def pick_strategy
-    strategy_klass = STRATEGIES.find { |klass| creator(klass).can_create?(@document_params) }
-    creator(strategy_klass).new @document_params
-  end
-
   def valid?
     @strategy.valid?
   end
@@ -18,7 +13,14 @@ class DocumentsCreator
     @strategy.save
   end
 
+private
+
   def creator(klass)
     "Creator::#{klass}".constantize
+  end
+
+  def pick_strategy
+    strategy_klass = STRATEGIES.find { |klass| creator(klass).can_create?(@document_params) }
+    creator(strategy_klass).new @document_params
   end
 end
