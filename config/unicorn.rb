c5a98c329f41c7a39a8924b9b00ea6708e49c64b
Damián Silvani <munshkr@gmail.com>
2013-08-19 19:29:46 -0300

    Extend Unicorn timeouts. Slowpoke

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,4 +6,4 @@ stdout_path "#{root}/log/unicorn.log"
 
 listen "/tmp/unicorn.mapa.sock"
 worker_processes 2
-timeout 30
+timeout 300
--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -32,7 +32,7 @@ listen "/tmp/.sock", :backlog => 64
 listen 8090, :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
-timeout 30
+timeout 300
 
 # feel free to point this anywhere accessible on the filesystem
 pid "#{app_path}/tmp/pids/unicorn.pid"
